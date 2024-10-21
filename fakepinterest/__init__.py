d08bfeb0fed6214b98734dddfa6a2f5299804d9e
Monsterned <andrewl.12.al85@gmail.com>
2024-10-20 21:03:40 -0300

    feito ajustes de base de dados online

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "29b86e03a02602bc9dd9515298351f27"
 app.config["UPLOAD_FOLDER"] = "fakepinterest/static/fotos_posts"
 
